10066d4b7b93bf5b68257e9f8006a03d246b8d5e
Bela Bertalan <belabertalan@Belas-MacBook-Pro.local>
2025-10-19 12:33:44 +0200

    feat: add Hero section with background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,36 @@
+import { Box, Typography, Button, Container, Stack, Card } from "@mui/material";
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundImage: "url(/images/hero.jpeg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "60vh",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Container>
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            textAlign="center"
+            spacing={7}
+            sx={{ height: "100%" }}
+          >
+            <Typography variant="h1" color="white">
+              Precision Cuts, Timeless Style
+            </Typography>
+            <Typography variant="h5" color="white">
+              Where classic barbering meets modern grooming
+            </Typography>
+            <Button variant="contained">Book Now</Button>
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
 }
